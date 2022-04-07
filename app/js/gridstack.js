5381e2ccb76ed1dbbc363d74201a69605e3f65bd
billz <billzimmerman@gmail.com>
2022-04-07 16:09:01 +0100

    Update grid.save, set saveContent = false

--- app/js/gridstack.js
+++ app/js/gridstack.js
@@ -1,4 +1,10 @@
 var options = {
+    animate: true,
+    acceptWidgets: true,
+    dragIn: '.newWidget', // class that can be dragged from outside
+    dragInOptions: { revert: 'invalid', scroll: false, appendTo: 'body', helper: 'clone' },
+    removable: '#trash', // drag-out delete class
+    removeTimeout: 100,
     column: 12,
     maxRow: 12,
     minRow: 1, // prevent grid from collapsing when empty
@@ -13,15 +19,15 @@ var grid = GridStack.init(options);
 
 // Save to LocalStorage
 grid.on('change', function (event, items) {
-    var serializedData = grid.save();
-    localStorage.setItem("grid-layout", JSON.stringify(serializedData, null, ' '));
+    var saveContent = false;
+    var serializedData = grid.save(saveContent);
+    localStorage.setItem("grid-layout", JSON.stringify(serializedData, null, '  '));
 });
 
 // Load from LocalStorage
 loadGrid = function() {
     if (serializedData = localStorage.getItem("grid-layout")) {
-        grid.removeAll();
-        grid.load(JSON.parse(serializedData), true);
+        grid.load(JSON.parse(serializedData)); // update widgets
     }
 }
 
