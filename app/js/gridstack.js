fd3484288e83598617aad31d6b21bb8fd91383f1
billz <billzimmerman@gmail.com>
2022-04-07 11:55:33 +0100

    Save / load grid-layout to localStorage

--- app/js/gridstack.js
+++ app/js/gridstack.js
@@ -1,13 +1,28 @@
 var options = {
-  // number of columns
-  column: 12,
-  // max/min number of rows
-  maxRow: 12,
-  animate: true,
-  alwaysShowResizeHandle: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
-  resizable: {
-    handles: 'e, se, s, sw, w, n'
-  }
+    column: 12,
+    maxRow: 12,
+    minRow: 1, // prevent grid from collapsing when empty
+    animate: true,
+    alwaysShowResizeHandle: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+    resizable: {
+        handles: 'e, se, s, sw, w, n'
+    }
 };
+
 var grid = GridStack.init(options);
 
+// Save to LocalStorage
+grid.on('change', function (event, items) {
+    var serializedData = grid.save();
+    localStorage.setItem("grid-layout", JSON.stringify(serializedData, null, ' '));
+});
+
+// Load from LocalStorage
+loadGrid = function() {
+    if (serializedData = localStorage.getItem("grid-layout")) {
+        grid.removeAll();
+        grid.load(JSON.parse(serializedData), true);
+    }
+}
+
+loadGrid();
